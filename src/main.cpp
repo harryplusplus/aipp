f6c23513c58da39a600d59688125b8b1a82eb7df
harryplusplus <harryplusplus@gmail.com>
2026-04-30 11:45:33 +0900

    Add ASCII art UI using FTXUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
-int main() { return 0; }
+#include <string>
+
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+
+int main() {
+    using namespace ftxui;
+    const std::string ascii_art = R"(
+   ___   ___
+  / _ \ / _ \
+ | (_) | (_) |
+  \___/ \___/
+   ++   ++
+)";
+    auto component = Renderer([&] { return text(ascii_art); });
+    auto screen = ScreenInteractive::Fullscreen();
+    screen.Loop(component);
+    return 0;
+}
